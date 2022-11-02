5192d1d420988c94fa4c9d037914b6b3feee8894
Rafael Araujo <rafa123839@gmail.com>
2022-11-02 19:25:24 -0300

    terminado layout da tela de login

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,17 +1,84 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
+import { Stack, IconButton, ActivityIndicator, Button } from "@react-native-material/core";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useNavigation } from "@react-navigation/core";
 const style = styles()
 
-export function SignIn () {
+export function SignIn() {
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const navigation = useNavigation()
+
     return (
         <View style={style.container}>
             <StatusBar backgroundColor="white" />
             <View style={style.logo}>
-                <Image style={style.image} source={require("./../../../assets/adaptive-icon3.png")}/>
-                <Text style={style.textWelcome}>Welcome to E-com</Text>
+                <Image style={style.image} source={require("./../../../assets/adaptive-icon3.png")} />
+                <Text style={style.textWelcome}>Bem-vindo ao E-com</Text>
                 <Text style={style.subtitle}>Sign in to continue</Text>
             </View>
+
+            <View style={style.containerLogin}>
+                <Stack>
+                    <View>
+                        <TextInput
+                            style={style.input}
+                            onChangeText={setEmail}
+                            value={email}
+                            placeholder="Your Email"
+                            keyboardType="email-address"
+                        />
+                        <MaterialCommunityIcons style={style.icon} name="email-outline" size={24} color="#9098B1" />
+                    </View>
+                    <View>
+                        <TextInput
+                            style={style.input}
+                            onChangeText={setSenha}
+                            value={senha}
+                            placeholder="Password"
+                            keyboardType="visible-password"
+                        />
+                        <MaterialCommunityIcons style={style.icon} name="account-key-outline" size={24} color="#9098B1" />
+                    </View>
+                    <TouchableOpacity style={style.botao} onPress={() => navigation.navigate('Home')}>
+                        <Text style={style.textBotao}>Sign in</Text>
+                    </TouchableOpacity>
+                </Stack>
+            </View>
+
+            <View style={style.orLine}>
+                <View style={style.line} />
+                <Text style={style.textOr}>OR</Text>
+                <View style={style.line} />
+            </View>
+
+            <View style={{ marginHorizontal: 16 }}>
+                <TouchableOpacity style={style.botaoLogarRedes}>
+                    <Text style={style.textBotaoRedes}>Login with Google</Text>
+                    <Image style={style.logoBotao} source={require('../../assets/Google.png')} />
+                </TouchableOpacity>
+                <TouchableOpacity style={style.botaoLogarRedes}>
+                    <Text style={style.textBotaoRedes}>Login with facebook</Text>
+                    <Image style={style.logoBotao} source={require('../../assets/Facebook.png')} />
+                </TouchableOpacity>
+            </View>
+
+            <View style={{ alignItems: 'center', marginTop: 20 }}>
+                <TouchableOpacity>
+                    <Text style={{ color: "#40BFFF", fontSize: 16, fontWeight: 'bold' }}>Forgot Password?</Text>
+                </TouchableOpacity>
+
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 10 }}>
+                    <Text style={{ color: '#9098B1' }}>Don’t have a account?</Text>
+                    <TouchableOpacity>
+                        <Text style={{ color: '#40BFFF' }}>Register</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
         </View>
     )
 }
--- src/screens/SignIn/styles.js
+++ src/screens/SignIn/styles.js
@@ -4,31 +4,110 @@ export const styles = () => {
     return StyleSheet.create({
         container: {
             flex: 1,
-            // paddingTop: 60,
+            justifyContent: 'center',
             marginTop: StatusBar.currentHeight || 0,
             backgroundColor: 'white',
-            
+
         },
         logo: {
-            flex: 1,
-            alignContent: 'center', 
+            alignContent: 'center',
             alignItems: 'center',
-            justifyContent: 'center'
-        }, 
+            justifyContent: 'center',
+            marginBottom: 40
+        },
         image: {
             width: 72,
             height: 72,
             borderRadius: 16,
             marginBottom: 8
-        }, 
+        },
         textWelcome: {
             fontSize: 18,
             fontWeight: 'bold',
             color: '#223263',
             marginBottom: 8
-        }, 
+        },
         subtitle: {
             color: '#9098B1'
+        },
+        containerLogin: {
+            marginHorizontal: 16,
+        },
+        input: {
+            height: 60,
+            paddingLeft: 55,
+            borderWidth: 1,
+            padding: 10,
+            borderWidth: 2,
+            borderColor: '#EBF0FF',
+            borderRadius: 5,
+            marginBottom: 10,
+            color: '#9098B1',
+            fontWeight: 'bold'
+        },
+        icon: {
+            position: 'absolute',
+            top: 15,
+            left: 18
+        },
+        botao: {
+            marginTop: 10,
+            backgroundColor: "#40BFFF",
+            height: 70,
+            justifyContent: 'center',
+            borderRadius: 5,
+            elevation: 20,
+            shadowColor: '#40BFFF',
+            shadowOffset: { width: -10, height: 20 },
+            shadowOpacity: 0.2,
+            shadowRadius: 3,
+        },
+        textBotao: {
+            textAlign: 'center',
+            justifyContent: 'center',
+            color: 'white',
+            fontWeight: 'bold',
+            fontSize: 16
+        }, 
+        orLine: {
+            marginTop: 20,
+            marginBottom: 10,
+            marginHorizontal: 16,
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center'
+        },
+        textOr: {
+            marginHorizontal: 10,
+            fontWeight: 'bold',
+            color: "#9098B1"
+        },
+        line: {
+            width: '41%', 
+            height: 2, 
+            backgroundColor: '#EBF0FF'
+        },
+        botaoLogarRedes: {
+            marginTop: 10,
+            height: 70,
+            justifyContent: 'center',
+            borderColor: '#EBF0FF',
+            borderRadius: 5,
+            borderWidth: 2
+        }, 
+        logoBotao: {
+            position: 'absolute',
+            width: 40,
+            height: 40,
+            // top: 15,
+            left: 16
+        },
+        textBotaoRedes: {
+            textAlign: 'center',
+            justifyContent: 'center',
+            color: '#9098B1',
+            fontWeight: 'bold',
+            fontSize: 16
         }
     })
 }
